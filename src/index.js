3bad0c930d9dce78f2e4f6e271977d9d362f3b07
CrioUser <crio-user@criodo.com>
2024-12-26 16:18:01 +0530

    ME_QKART_BACKEND_MODULE_UNDERSTANDING_BASICS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 const mongoose = require("mongoose");
 const app = require("./app");
 const config = require("./config/config");
@@ -8,9 +7,11 @@ let server;
 // TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Create Mongo connection and get the express app to listen on config.port
 mongoose
   .connect(config.mongoose.url)
-  .then(() => console.log("Connected to DB at", config.mongoose.url))
-  .catch((error) => console.log("Failed to connect to DB\n", error));
+  .then(() => {
+    console.log("Connected to DB at", config.mongoose.url);
 
-app.listen(config.port, () => {
-  console.log("Server Listening at", config.port);
-});
+    app.listen(config.port, () => {
+      console.log("Server Listening at", config.port);
+    });
+  })
+  .catch((error) => console.log("Failed to connect to DB\n", error));
